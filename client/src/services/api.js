9405866c72ecba2c28c5ab85a05cd110b2c7e2e7
normanchingcuanco <norman.chingcuanco@gmail.com>
2026-03-02 16:57:10 +0800

    Vercel hosting

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api"
+  baseURL: import.meta.env.VITE_API_URL
 })
 
-// Attach token automatically
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token")
   if (token) {
